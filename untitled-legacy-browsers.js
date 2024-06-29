fc8fc772c47933836f372eb5136e464d6fbd7374
Maria Orfanelli <66322499+mariaorfanelli@users.noreply.github.com>
2024-06-29 18:07:59 -0300

    Add files via upload

---
+++ untitled-legacy-browsers.js
@@ -0,0 +1,392 @@
+﻿/***************** 
+ * Untitled *
+ *****************/
+
+
+// store info about the experiment session:
+let expName = 'untitled';  // from the Builder filename that created this script
+let expInfo = {
+    'participant': `${util.pad(Number.parseFloat(util.randint(0, 999999)).toFixed(0), 6)}`,
+    'session': '001',
+};
+
+// Start code blocks for 'Before Experiment'
+// init psychoJS:
+const psychoJS = new PsychoJS({
+  debug: true
+});
+
+// open window:
+psychoJS.openWindow({
+  fullscr: true,
+  color: new util.Color([0,0,0]),
+  units: 'height',
+  waitBlanking: true,
+  backgroundImage: '',
+  backgroundFit: 'none',
+});
+// schedule the experiment:
+psychoJS.schedule(psychoJS.gui.DlgFromDict({
+  dictionary: expInfo,
+  title: expName
+}));
+
+const flowScheduler = new Scheduler(psychoJS);
+const dialogCancelScheduler = new Scheduler(psychoJS);
+psychoJS.scheduleCondition(function() { return (psychoJS.gui.dialogComponent.button === 'OK'); }, flowScheduler, dialogCancelScheduler);
+
+// flowScheduler gets run if the participants presses OK
+flowScheduler.add(updateInfo); // add timeStamp
+flowScheduler.add(experimentInit);
+flowScheduler.add(trialRoutineBegin());
+flowScheduler.add(trialRoutineEachFrame());
+flowScheduler.add(trialRoutineEnd());
+flowScheduler.add(quitPsychoJS, '', true);
+
+// quit if user presses Cancel in dialog box:
+dialogCancelScheduler.add(quitPsychoJS, '', false);
+
+psychoJS.start({
+  expName: expName,
+  expInfo: expInfo,
+  resources: [
+    // resources:
+    {'name': 'german_adult.mp3', 'path': 'german_adult.mp3'},
+  ]
+});
+
+psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.WARNING);
+
+
+var currentLoop;
+var frameDur;
+async function updateInfo() {
+  currentLoop = psychoJS.experiment;  // right now there are no loops
+  expInfo['date'] = util.MonotonicClock.getDateStr();  // add a simple timestamp
+  expInfo['expName'] = expName;
+  expInfo['psychopyVersion'] = '2024.1.5';
+  expInfo['OS'] = window.navigator.platform;
+
+
+  // store frame rate of monitor if we can measure it successfully
+  expInfo['frameRate'] = psychoJS.window.getActualFrameRate();
+  if (typeof expInfo['frameRate'] !== 'undefined')
+    frameDur = 1.0 / Math.round(expInfo['frameRate']);
+  else
+    frameDur = 1.0 / 60.0; // couldn't get a reliable measure so guess
+
+  // add info from the URL:
+  util.addInfoFromUrl(expInfo);
+  
+
+  
+  psychoJS.experiment.dataFileName = (("." + "/") + `data/${expInfo["participant"]}_${expName}_${expInfo["date"]}`);
+  psychoJS.experiment.field_separator = '\t';
+
+
+  return Scheduler.Event.NEXT;
+}
+
+
+var trialClock;
+var text;
+var sound_1;
+var button_2;
+var var_3;
+var globalClock;
+var routineTimer;
+async function experimentInit() {
+  // Initialize components for Routine "trial"
+  trialClock = new util.Clock();
+  text = new visual.TextStim({
+    win: psychoJS.window,
+    name: 'text',
+    text: 'ok. ouça.',
+    font: 'Arial',
+    units: undefined, 
+    pos: [0, 0], height: 0.05,  wrapWidth: undefined, ori: 0.0,
+    languageStyle: 'LTR',
+    color: new util.Color('white'),  opacity: undefined,
+    depth: 0.0 
+  });
+  
+  sound_1 = new sound.Sound({
+      win: psychoJS.window,
+      value: 'A',
+      secs: (- 1),
+      });
+  sound_1.setVolume(1.0);
+  button_2 = new visual.ButtonStim({
+    win: psychoJS.window,
+    name: 'button_2',
+    text: '2',
+    fillColor: 'darkgrey',
+    borderColor: null,
+    color: 'white',
+    colorSpace: 'rgb',
+    pos: [0, 0],
+    letterHeight: 0.05,
+    size: [0.5, 0.5],
+    depth: -2
+  });
+  button_2.clock = new util.Clock();
+  
+  var_3 = new visual.ButtonStim({
+    win: psychoJS.window,
+    name: 'var_3',
+    text: '1',
+    fillColor: 'darkgrey',
+    borderColor: null,
+    color: 'white',
+    colorSpace: 'rgb',
+    pos: [(- 0.5), 0],
+    letterHeight: 0.05,
+    size: [0.5, 0.5],
+    depth: -3
+  });
+  var_3.clock = new util.Clock();
+  
+  // Create some handy timers
+  globalClock = new util.Clock();  // to track the time since experiment started
+  routineTimer = new util.CountdownTimer();  // to track time remaining of each (non-slip) routine
+  
+  return Scheduler.Event.NEXT;
+}
+
+
+var t;
+var frameN;
+var continueRoutine;
+var trialComponents;
+function trialRoutineBegin(snapshot) {
+  return async function () {
+    TrialHandler.fromSnapshot(snapshot); // ensure that .thisN vals are up to date
+    
+    //--- Prepare to start Routine 'trial' ---
+    t = 0;
+    trialClock.reset(); // clock
+    frameN = -1;
+    continueRoutine = true; // until we're told otherwise
+    routineTimer.add(21.000000);
+    // update component parameters for each repeat
+    psychoJS.experiment.addData('trial.started', globalClock.getTime());
+    sound_1.setValue('german_adult.mp3');
+    sound_1.secs=5;
+    sound_1.setVolume(1.0);
+    // reset button_2 to account for continued clicks & clear times on/off
+    button_2.reset()
+    // reset var_3 to account for continued clicks & clear times on/off
+    var_3.reset()
+    // keep track of which components have finished
+    trialComponents = [];
+    trialComponents.push(text);
+    trialComponents.push(sound_1);
+    trialComponents.push(button_2);
+    trialComponents.push(var_3);
+    
+    trialComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent)
+        thisComponent.status = PsychoJS.Status.NOT_STARTED;
+       });
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+var frameRemains;
+function trialRoutineEachFrame() {
+  return async function () {
+    //--- Loop for each frame of Routine 'trial' ---
+    // get current time
+    t = trialClock.getTime();
+    frameN = frameN + 1;// number of completed frames (so 0 is the first frame)
+    // update/draw components on each frame
+    
+    // *text* updates
+    if (t >= 0.0 && text.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      text.tStart = t;  // (not accounting for frame time here)
+      text.frameNStart = frameN;  // exact frame index
+      
+      text.setAutoDraw(true);
+    }
+    
+    frameRemains = 0.0 + 5 - psychoJS.window.monitorFramePeriod * 0.75;  // most of one frame period left
+    if (text.status === PsychoJS.Status.STARTED && t >= frameRemains) {
+      text.setAutoDraw(false);
+    }
+    
+    // start/stop sound_1
+    if (t >= 5 && sound_1.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      sound_1.tStart = t;  // (not accounting for frame time here)
+      sound_1.frameNStart = frameN;  // exact frame index
+      
+      psychoJS.window.callOnFlip(function(){ sound_1.play(); });  // screen flip
+      sound_1.status = PsychoJS.Status.STARTED;
+    }
+    frameRemains = 5 + 5 - psychoJS.window.monitorFramePeriod * 0.75;  // most of one frame period left
+    if (sound_1.status === PsychoJS.Status.STARTED && t >= frameRemains) {
+      if (t >= sound_1.tStart + 0.5) {
+        sound_1.stop();  // stop the sound (if longer than duration)
+        sound_1.status = PsychoJS.Status.FINISHED;
+      }
+    }
+    
+    // *button_2* updates
+    if (t >= 11 && button_2.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      button_2.tStart = t;  // (not accounting for frame time here)
+      button_2.frameNStart = frameN;  // exact frame index
+      
+      button_2.setAutoDraw(true);
+    }
+    
+    frameRemains = 11 + 10 - psychoJS.window.monitorFramePeriod * 0.75;  // most of one frame period left
+    if (button_2.status === PsychoJS.Status.STARTED && t >= frameRemains) {
+      button_2.setAutoDraw(false);
+    }
+    
+    if (button_2.status === PsychoJS.Status.STARTED) {
+      // check whether button_2 has been pressed
+      if (button_2.isClicked) {
+        if (!button_2.wasClicked) {
+          // store time of first click
+          button_2.timesOn.push(button_2.clock.getTime());
+          // store time clicked until
+          button_2.timesOff.push(button_2.clock.getTime());
+        } else {
+          // update time clicked until;
+          button_2.timesOff[button_2.timesOff.length - 1] = button_2.clock.getTime();
+        }
+        if (!button_2.wasClicked) {
+          // end routine when button_2 is clicked
+          continueRoutine = false;
+          
+        }
+        // if button_2 is still clicked next frame, it is not a new click
+        button_2.wasClicked = true;
+      } else {
+        // if button_2 is clicked next frame, it is a new click
+        button_2.wasClicked = false;
+      }
+    } else {
+      // keep clock at 0 if button_2 hasn't started / has finished
+      button_2.clock.reset();
+      // if button_2 is clicked next frame, it is a new click
+      button_2.wasClicked = false;
+    }
+    
+    // *var_3* updates
+    if (t >= 11 && var_3.status === PsychoJS.Status.NOT_STARTED) {
+      // keep track of start time/frame for later
+      var_3.tStart = t;  // (not accounting for frame time here)
+      var_3.frameNStart = frameN;  // exact frame index
+      
+      var_3.setAutoDraw(true);
+    }
+    
+    frameRemains = 11 + 10 - psychoJS.window.monitorFramePeriod * 0.75;  // most of one frame period left
+    if (var_3.status === PsychoJS.Status.STARTED && t >= frameRemains) {
+      var_3.setAutoDraw(false);
+    }
+    
+    if (var_3.status === PsychoJS.Status.STARTED) {
+      // check whether var_3 has been pressed
+      if (var_3.isClicked) {
+        if (!var_3.wasClicked) {
+          // store time of first click
+          var_3.timesOn.push(var_3.clock.getTime());
+          // store time clicked until
+          var_3.timesOff.push(var_3.clock.getTime());
+        } else {
+          // update time clicked until;
+          var_3.timesOff[var_3.timesOff.length - 1] = var_3.clock.getTime();
+        }
+        if (!var_3.wasClicked) {
+          // end routine when var_3 is clicked
+          continueRoutine = false;
+          
+        }
+        // if var_3 is still clicked next frame, it is not a new click
+        var_3.wasClicked = true;
+      } else {
+        // if var_3 is clicked next frame, it is a new click
+        var_3.wasClicked = false;
+      }
+    } else {
+      // keep clock at 0 if var_3 hasn't started / has finished
+      var_3.clock.reset();
+      // if var_3 is clicked next frame, it is a new click
+      var_3.wasClicked = false;
+    }
+    // check for quit (typically the Esc key)
+    if (psychoJS.experiment.experimentEnded || psychoJS.eventManager.getKeys({keyList:['escape']}).length > 0) {
+      return quitPsychoJS('The [Escape] key was pressed. Goodbye!', false);
+    }
+    
+    // check if the Routine should terminate
+    if (!continueRoutine) {  // a component has requested a forced-end of Routine
+      return Scheduler.Event.NEXT;
+    }
+    
+    continueRoutine = false;  // reverts to True if at least one component still running
+    trialComponents.forEach( function(thisComponent) {
+      if ('status' in thisComponent && thisComponent.status !== PsychoJS.Status.FINISHED) {
+        continueRoutine = true;
+      }
+    });
+    
+    // refresh the screen if continuing
+    if (continueRoutine && routineTimer.getTime() > 0) {
+      return Scheduler.Event.FLIP_REPEAT;
+    } else {
+      return Scheduler.Event.NEXT;
+    }
+  };
+}
+
+
+function trialRoutineEnd(snapshot) {
+  return async function () {
+    //--- Ending Routine 'trial' ---
+    trialComponents.forEach( function(thisComponent) {
+      if (typeof thisComponent.setAutoDraw === 'function') {
+        thisComponent.setAutoDraw(false);
+      }
+    });
+    psychoJS.experiment.addData('trial.stopped', globalClock.getTime());
+    sound_1.stop();  // ensure sound has stopped at end of Routine
+    psychoJS.experiment.addData('button_2.numClicks', button_2.numClicks);
+    psychoJS.experiment.addData('button_2.timesOn', button_2.timesOn);
+    psychoJS.experiment.addData('button_2.timesOff', button_2.timesOff);
+    psychoJS.experiment.addData('var_3.numClicks', var_3.numClicks);
+    psychoJS.experiment.addData('var_3.timesOn', var_3.timesOn);
+    psychoJS.experiment.addData('var_3.timesOff', var_3.timesOff);
+    // Routines running outside a loop should always advance the datafile row
+    if (currentLoop === psychoJS.experiment) {
+      psychoJS.experiment.nextEntry(snapshot);
+    }
+    return Scheduler.Event.NEXT;
+  }
+}
+
+
+function importConditions(currentLoop) {
+  return async function () {
+    psychoJS.importAttributes(currentLoop.getCurrentTrial());
+    return Scheduler.Event.NEXT;
+    };
+}
+
+
+async function quitPsychoJS(message, isCompleted) {
+  // Check for and save orphaned data
+  if (psychoJS.experiment.isEntryEmpty()) {
+    psychoJS.experiment.nextEntry();
+  }
+  psychoJS.window.close();
+  psychoJS.quit({message: message, isCompleted: isCompleted});
+  
+  return Scheduler.Event.QUIT;
+}
